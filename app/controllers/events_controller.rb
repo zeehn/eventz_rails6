20acf4a611f6263a4a2fd7621f759bdcc108354c
Zakir Hussain <zack.mesacc@gmail.com>
2020-11-20 03:28:41 +0500

    Add new and create actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,11 +3,39 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def new 
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event 
+    else
+      render :new 
+    end
+  end
+
   def show
     @event = Event.find(params[:id])
   end
 
   def edit
     @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      flash[:notice] = 'Event updated successfully'
+      redirect_to @event
+    else
+      render :edit
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :price, :starts_at)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'events#index'
   get 'events', to: 'events#index'
+  post 'events', to: 'events#create'
+  get 'events/new', to: 'events#new', as: 'new_event'
   get 'events/:id', to: 'events#show', as: 'event'
   get 'events/:id/edit', to: 'events#edit', as: 'edit_event'
+  patch 'events/:id', to: 'events#update'
 end
