8dbcd99dc056e8c408195bb76a51cc98d7901148
Zakir Hussain <zack.mesacc@gmail.com>
2020-11-19 23:51:08 +0500

    Add edit route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'events#index'
-  get 'events/:id', to: 'events#show'
+  get 'events', to: 'events#index'
+  get 'events/:id', to: 'events#show', as: 'event'
+  get 'events/:id/edit', to: 'events#edit', as: 'edit_event'
 end
